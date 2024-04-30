d6f67bdd38ff418c94c17884c1d449203e0a1e33
eerojantunen <jant.eero@gmail.com>
2024-04-30 15:57:34 +0300

    Add login/signup function

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
-from flask import render_template
-import topics, threads, messages
+from flask import render_template, request, redirect
+import topics, threads, messages, users
 
 @app.route("/")
 def index():
@@ -16,3 +16,33 @@ def topic(id):
 def thread(id):
     messages_data = messages.get_list(id)
     return render_template("messages.html",messages_data=messages_data)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if users.login(username,password):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Wrong username or password")
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 == password2:
+            if users.signup(username,password1):
+                return redirect("/")
+            else:
+                return render_template("error.html",message = "Username taken")
+        else:
+            return render_template("error.html", message="Passwords don't match")
+
+@app.route("/logout")     
+def logout():
+       users.logout()
+       return redirect("/")
---
+++ users.py
@@ -0,0 +1,31 @@
+from db import db
+from sqlalchemy import text
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(username,password):
+    sql = text("SELECT id, password from users where username=:username")
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value,password):
+            session["username"] = username
+            return True
+        else:
+            return False
+
+def signup(username, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = text("insert into users (username, password) values (:username,:password)")
+        db.session.execute(sql, {"username":username,"password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+def logout():
+    del session["username"]
